65f8fe28ccde587525e0ee44a8e551eff171ac06
VendaGamer <wyjeme@seznam.cz>
2023-12-04 14:02:36 +0100

    added transparent object to indicate position where you'll place obstacle

--- Assets/Scripts/AddObstacle.cs
+++ Assets/Scripts/AddObstacle.cs
@@ -1,24 +1,42 @@
 using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.UIElements;
 
 public class AddObstacle : MonoBehaviour
 {
     Camera cam;
     [SerializeField]
     Rigidbody obstacle;
+    [SerializeField]
+    GameObject transparent_obst;
+    Vector3 old;
+    Vector3[,] position = new Vector3[4,6];
     void Start()
     {
+        for (int i = 0;i<4;i++)
+        {
+            for(int j = 0; j < 6; j++)
+            {
+                
+            }
+        }
         cam = this.GetComponent<Camera>();
+        transparent_obst = Instantiate(transparent_obst, transparent_obst.transform.position, Quaternion.identity);
     }
 
     void Update()
     {
-        if (Input.GetMouseButtonDown(1))
+
+        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out RaycastHit hit, 100)&& old !=hit.point )
         {
-            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
-            if (Physics.Raycast(ray, out RaycastHit hit, 100))
-            {
-                Instantiate(obstacle,hit.point,Quaternion.identity);
-            }
+            old = hit.point;
+            transparent_obst.transform.position = new Vector3(hit.point.x, transparent_obst.transform.position.y, hit.point.z);
+        }
+        if(Input.GetMouseButtonDown(1))
+        {
+            Instantiate(obstacle, hit.point, Quaternion.identity);
         }
     }
 }
