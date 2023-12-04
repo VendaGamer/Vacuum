bb5c38d1b1c56569f14cd8aa9fbbe71df260dccb
VendaGamer <wyjeme@seznam.cz>
2023-12-04 19:59:48 +0100

    better obstacle placing

--- Assets/Scripts/AddObstacle.cs
+++ Assets/Scripts/AddObstacle.cs
@@ -1,42 +1,65 @@
+using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.AI;
 using UnityEngine.UIElements;
 
 public class AddObstacle : MonoBehaviour
 {
-    Camera cam;
+    private Camera cam;
     [SerializeField]
-    Rigidbody obstacle;
+    private Rigidbody obstacle;
     [SerializeField]
-    GameObject transparent_obst;
-    Vector3 old;
-    Vector3[,] position = new Vector3[4,6];
+    private GameObject transparent_obst;
+    private Vector3 old;
+    private List<Rigidbody> obstacles = new List<Rigidbody>();
     void Start()
     {
-        for (int i = 0;i<4;i++)
-        {
-            for(int j = 0; j < 6; j++)
-            {
-                
-            }
-        }
         cam = this.GetComponent<Camera>();
-        transparent_obst = Instantiate(transparent_obst, transparent_obst.transform.position, Quaternion.identity);
+        transparent_obst = Instantiate(transparent_obst, new Vector3(0,-10,0), Quaternion.identity);
     }
 
     void Update()
     {
 
         Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+        if(Physics.Raycast(ray, out RaycastHit hit, 100)&& hit.point.x<=16.2&& hit.point.x >= -16.2
+            && hit.point.z <= 10 && hit.point.z >= -10 && obstacles.Count!=0) {
+            if (Colliding(hit.point, 4))
+            {
+                if (old != hit.point && hit.collider.gameObject.tag != "Obstacle")
+                {
+                    var boi= new Vector3(hit.point.x, 0, hit.point.z);
+                    old = boi;
+                    transparent_obst.transform.position = boi;
+                }
+                if(Input.GetMouseButtonDown(1))
+                {
+                    var o = Instantiate(obstacle, new Vector3(hit.point.x, 0, hit.point.z), Quaternion.identity);
+                    obstacles.Add(o);
+                }
+            }
+            else if(Input.GetMouseButtonDown(1)) {
+                var o = Instantiate(obstacle, new Vector3(old.x, 0, old.z), Quaternion.identity);
+                obstacles.Add(o);
+            }
 
-        if (Physics.Raycast(ray, out RaycastHit hit, 100)&& old !=hit.point )
-        {
-            old = hit.point;
-            transparent_obst.transform.position = new Vector3(hit.point.x, transparent_obst.transform.position.y, hit.point.z);
         }
-        if(Input.GetMouseButtonDown(1))
+    }
+
+    private bool Colliding(Vector3 position, float minDistance)
+    {
+        if(obstacles.Count == 0)
         {
-            Instantiate(obstacle, hit.point, Quaternion.identity);
+            return true;
         }
+        foreach (var existingPosition in obstacles)
+        {
+            if (Vector3.Distance(position, existingPosition.transform.position) < minDistance)
+            {
+                return false;
+            }
+        }
+        return true;
     }
 }
