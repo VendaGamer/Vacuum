ce85597d35f82fc8957c28e6dc3c6443330838d0
VendaGamer <wyjeme@seznam.cz>
2023-12-05 22:20:17 +0100

    m

--- Assets/Scripts/Scenator.cs
+++ Assets/Scripts/Scenator.cs
@@ -5,17 +5,14 @@ using UnityEngine.SceneManagement;
 public class Scenator : MonoBehaviour
 {
     private int current = 0;
-    public void Reset()
-    {
-        SceneManager.LoadScene(current);
-    }
+
     public void LoadNext()
     {
         SceneManager.LoadScene(2);
         current++;
         SceneManager.LoadScene(2+current, LoadSceneMode.Additive);
     }
-    public void Death()
+    public void Reset()
     {
         SceneManager.LoadScene(2);
         SceneManager.LoadScene(2 + current, LoadSceneMode.Additive);
